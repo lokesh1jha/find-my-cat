f54679e7ebc7ab34bf25c12bee4e2cddb88a79c5
lokeshjha <lokesh1jha@gmai.com>
2024-11-11 23:21:04 +0530

    hyderation issue fixed

---
+++ utils/useIsMounted.ts
@@ -0,0 +1,16 @@
+
+import { useEffect, useState } from "react";
+
+// to fix the hyderation issue
+
+export default function useIsMounted() {
+  const [mounted, setMounted] = useState(false);
+
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+
+  return mounted;
+}
